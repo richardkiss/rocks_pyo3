8d5a8ce5ebfdd5f9a81074fb7a8b84889c5d57dc
Richard Kiss <him@richardkiss.com>
2025-02-20 12:39:04 -0800

    debian-9

--- .github/scripts/before-script-linux-debian-9.sh
+++ .github/scripts/before-script-linux-debian-9.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-echo "Installing libclang for Ubuntu/Debian..."
+echo "Installing libclang for Debian 9..."
+
+echo "BEFORE"
+cat /etc/apt/sources.list
+# debian 9 is EOL so we have to tweak the sources.list
+sed -i 's/http:\/\/deb.debian.org\/debian/http:\/\/archive.debian.org\/debian/g' /etc/apt/sources.list
+sed -i 's/http:\/\/security.debian.org\/debian/http:\/\/archive.debian.org\/debian/g' /etc/apt/sources.list
+
+echo "AFTER"
+cat /etc/apt/sources.list
+
 /usr/bin/apt update
 /usr/bin/apt install -y clang libclang-dev build-essential libc6-dev-i386
 export LIBCLANG_PATH=/usr/lib/llvm-14/lib
