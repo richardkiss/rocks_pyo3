ff6f10d037ca3446a66bb39fecda5a8024ed8d88
Richard Kiss <him@richardkiss.com>
2025-02-21 17:36:27 -0800

    just i686

--- .github/scripts/before-script-linux-almalinux-8.10.sh
+++ .github/scripts/before-script-linux-almalinux-8.10.sh
@@ -2,14 +2,12 @@
 
 echo 'Alma linux'
 
-/usr/bin/yum install -y clang-devel
+#/usr/bin/yum install -y clang-devel
 
 #/usr/bin/yum install -y python3-pip
 python3 -m ensurepip --upgrade
 ln /usr/local/bin/pip3 /usr/local/bin/pip
 
-dnf install -y gcc gcc-c++ glibc-devel.i686
-
-dnf install -y tmate
-
-script -q -c "tmate" /dev/null
+#dnf install -y gcc gcc-c++ glibc-devel.i686
+#dnf install -y tmate
+#script -q -c "tmate" /dev/null
