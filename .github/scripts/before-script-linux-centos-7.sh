7daab9954b21a7aa1d3b31060266e03cb854681f
Richard Kiss <him@richardkiss.com>
2025-08-06 17:57:27 -0700

    more centos deps

--- .github/scripts/before-script-linux-centos-7.sh
+++ .github/scripts/before-script-linux-centos-7.sh
@@ -6,4 +6,11 @@ echo "Running before-script for centos-7..."
 # Install 32-bit cross-compiler and dependencies for i686 builds
 yum install -y gcc gcc-c++ glibc-devel.i686 libgcc.i686
 
-echo "Finished installing i686 cross-compiler dependencies."
+# Install clang and libclang for bindgen
+yum install -y clang clang-devel
+
+# Set LIBCLANG_PATH for bindgen
+export LIBCLANG_PATH=/usr/lib64
+echo "LIBCLANG_PATH=$LIBCLANG_PATH"
+
+echo "Finished installing dependencies."
