386a5471233b08d7f7b457139d0eea944ada9e5d
Richard Kiss <him@richardkiss.com>
2025-08-19 16:42:04 -0700

    build

--- .github/scripts/before-script-linux-almalinux-8.10.sh
+++ .github/scripts/before-script-linux-almalinux-8.10.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-echo 'Alma linux'
+set -e
 
-#/usr/bin/yum install -y clang-devel
+echo "Running before-script for almalinux-8.10..."
 
-#/usr/bin/yum install -y python3-pip
+# Enable 32-bit architecture support
+dnf install -y glibc-devel.i686 libgcc.i686
+
+# Install C/C++ compilers
+dnf install -y gcc gcc-c++
+
+# Install clang and its development libraries
+dnf install -y clang clang-devel
+
+# Set LIBCLANG_PATH for bindgen
+export LIBCLANG_PATH=/usr/lib64
+echo "LIBCLANG_PATH=$LIBCLANG_PATH"
+
 python3 -m ensurepip --upgrade
 ln /usr/local/bin/pip3 /usr/local/bin/pip
 
-#dnf install -y gcc gcc-c++ glibc-devel.i686
-#dnf install -y tmate
-#script -q -c "tmate" /dev/null
+echo "Finished installing dependencies."
